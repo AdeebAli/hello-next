349781fb02c601df324fdbb3f709c891ab9f337d
Adeeb Ali <adeebali06gmail.com>
2020-03-29 15:36:46 -0500

    create dynamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Layout from '../components/myLayout';
+import Link from 'next/link';
 
-const Index = () => {
+const PostLink = props => (
+	<li>
+		<Link href={`/post?title=${props.title}`}>
+			<a>{props.title}</a>
+		</Link>
+	</li>
+);
+const Blog = () => {
 	return (
 		<Layout>
-			<p>Hello Next.js</p>
+			<h1>My Blog</h1>
+			<ul>
+				<PostLink title="Hello Next.js"/>
+				<PostLink title="Learn Next.js is awesome"/>
+				<PostLink title="Deploy apps with Zeit"/>
+			</ul>
 		</Layout>
-
 	);
 };
 
-export default Index;
+export default Blog;
