28b7061deecb580311ba778dce7ea199f3ecf5bd
Adeeb Ali <22379775+AdeebAli@users.noreply.github.com>
2020-03-29 21:21:17 -0500

    add api routes

--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,57 @@
-import Layout from '../components/myLayout';
-import Link from 'next/link';
+import useSWR from 'swr';
+import {useRouter} from 'next/router';
+const fetcher = url => {
+	/* eslint-disable no-undef */
+	return fetch(url).then(r => r.json());
+	/* eslint-enable no-undef */
+};
 
-function getPosts() {
-	return [
-		{id: 'hello-nextjs', title: 'Hello Next.js'},
-		{id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
-		{id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'}
-	];
-}
+const Index = () => {
+	const {query} = useRouter();
+	const {data, error} = useSWR(
+		`/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+		fetcher
+	);
+	// The following line has optional chaining, added in Next.js v9.1.5,
+	// is the same as `data && data.author`
+	const author = data?.author;
+	let quote = data?.quote;
 
-const PostLink = ({post}) => (
-	<li>
-		<Link href="/p/[id]" as={`/p/${post.id}`}>
-			<a>{post.title}</a>
-		</Link>
-		<style jsx>{`
-			li {
-				list-style: none;
-				margin: 5px 0;
-			}
+	if (!data) {
+		quote = 'Loading...';
+	}
 
-			a {
-				text-decoration: none;
-				color: blue;
-				font-family: 'Arial';
-			}
+	if (error) {
+		quote = 'Failed to fetch the quote.';
+	}
 
-			a:hover {
-				opacity: 0.6;
-			}
-			`}
-		</style>
-	</li>
-);
-
-const Blog = () => {
 	return (
-		<Layout>
-			<h1>My Blog</h1>
-			<ul>
-				{getPosts().map(post => (
-					<PostLink key={post.id} post={post}/>
-				))}
-			</ul>
+		<main className="center">
+			<div className="quote">{quote}</div>
+			{author && <span className="author">- {author}</span>}
+
 			<style jsx>{`
-        h1,
-        a {
-          font-family: 'Arial';
-        }
-
-        ul {
-          padding: 0;
-        }
-
-        li {
-          list-style: none;
-          margin: 5px 0;
-        }
-
-        a {
-          text-decoration: none;
-          color: blue;
-        }
-
-        a:hover {
-          opacity: 0.6;
-        }
-      `}
+		  main {
+			width: 90%;
+			max-width: 900px;
+			margin: 300px auto;
+			text-align: center;
+		  }
+		  .quote {
+			font-family: cursive;
+			color: #e243de;
+			font-size: 24px;
+			padding-bottom: 10px;
+		  }
+		  .author {
+			font-family: sans-serif;
+			color: #559834;
+			font-size: 20px;
+		  }
+		`}
 			</style>
-		</Layout>
+		</main>
 	);
 };
 
-export default Blog;
+export default Index;
